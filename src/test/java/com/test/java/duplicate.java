79d57b2bef58d7eb5dd1aaa83240d2c829ef9de3
Sanny Anand <sanny12012@gmail.com>
2026-02-07 19:48:53 +0530

    Test

--- src/test/java/com/test/java/duplicate.java
+++ src/test/java/com/test/java/duplicate.java
@@ -1,21 +1,26 @@
 package com.test.java;
 
-import java.util.Scanner;
+import org.junit.jupiter.api.Test;
 
 public class duplicate {
-	public static void main(String[] args) {
-		int arr[] = { 1, 1, 2, 2, 5, 7, 7 };
-		System.out.println("Duplicate element in array:");
 
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] == arr[j]) {
-					System.out.print(arr[i] + " ");
-					break;
-				}
+    @Test
+    void printDuplicateElements() {
 
-			}
-		}
+        int[] arr = {1, 1, 2, 2, 5, 7, 7};
 
-	}
+        System.out.println("Duplicate elements in array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Duplicate test executed successfully");
+    }
 }
